4e1fd41793e87027ab6b23c4d60178c5e7413cb0
winterTTr <winterTTr@gmail.com>
2011-06-15 15:37:17 +0800

    config basic org mode setting

--- .emacs.d/my-settings/my-org-mode-setting.el
+++ .emacs.d/my-settings/my-org-mode-setting.el
@@ -1,9 +1,11 @@
 ;; -*- coding: utf-8 -*-
 
 ;; org-mode setting
+(require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
+(setq org-agenda-files (list "O:/gomez.org") )
+(setq org-log-done t)
 
 (provide 'my-org-mode-setting)
