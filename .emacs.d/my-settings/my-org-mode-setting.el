0dbf5cf4345366239c46656cc19bc3d7840a0c70
winterTTr <winterTTr@gmail.com>
2011-06-16 22:23:36 +0800

    Custom org-mode, add face color for specific todo status

--- .emacs.d/my-settings/my-org-mode-setting.el
+++ .emacs.d/my-settings/my-org-mode-setting.el
@@ -5,7 +5,32 @@
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
-(setq org-agenda-files (list "O:/gomez.org") )
+(setq org-agenda-files
+      (list
+       "E:/My Dropbox/Material/Org/gomez.org") )
 (setq org-log-done t)
+(setq org-insert-mode-line-in-empty-file t)
+
+
+(setq org-todo-keyword-faces
+      '(
+        ;; bug workflow
+        ("OPEN" . (:foreground "PeachPuff" ) )
+        ("REOPEN" . (:foreground "PeachPuff" ) )
+        ("INVESTIGATE" . (:foreground "blue") )
+        ("BLOCK" .  org-warning)
+        ("PENDING" . (:foreground "GreenYellow"))
+        ("FIXED" . (:foreground "green" :weight bold))
+        ("WAD" .  (:foreground "green" :weight bold) )
+        ;; type workflow
+        ("Paul"   (:foreground "DarkSlateGray" ) )
+        ("Hunter"   (:foreground "DarkSlateGray" ) )
+        ("Drizzt"   (:foreground "DarkSlateGray" ) )
+        ("Sean"   (:foreground "DarkSlateGray" ) )
+        ("Kirill"   (:foreground "DarkSlateGray" ) )
+        ;; todo workflow
+        ("TODO" . (:foreground "PeachPuff") )
+        ("DONE" . (:foreground "green" :weight bold) )
+        )) 
 
 (provide 'my-org-mode-setting)
