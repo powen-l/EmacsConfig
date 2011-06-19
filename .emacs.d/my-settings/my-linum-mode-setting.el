0393c7439b244fc56030f87a08219a618e7dcfbe
winterTTr <winterTTr@gmail.com>
2011-06-19 19:56:56 +0800

    use addadvice to hack the linum-on function, make it ok to turn off on specific mode

--- .emacs.d/my-settings/my-linum-mode-setting.el
+++ .emacs.d/my-settings/my-linum-mode-setting.el
@@ -2,10 +2,26 @@
 
 ;; line number
 (require 'linum)
-(global-linum-mode t) 
 (setq linum-format "%4d| ")
 
+(setq linum-mode-inhibit-modes-list
+      '(help-mode
+        eshell-mode
+        shell-mode
+        erc-mode
+        jabber-roster-mode
+        jabber-chat-mode
+        gnus-group-mode
+        gnus-summary-mode
+        gnus-article-mode))
 
+(defadvice linum-on (around linum-on-inhibit-for-modes)
+  "stop the load of linum mode for some major mode"
+  (unless (member major-mode linum-mode-inhibit-modes-list)
+    ad-do-it))
 
+(ad-activate 'linum-on)
+
+(global-linum-mode 1) 
 
 (provide 'my-linum-mode-setting)
