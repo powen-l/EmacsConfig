97e7b186b2a63e016c31b55e40b530cb023e84d3
winterTTr <winterTTr@gmail.com>
2011-07-12 18:00:00 +0800

    easy-motion-mode: fix bug, the overlay vairable should be set to nil when done.

--- .emacs.d/plugins/easy-motion-mode/easy-motion-mode.el
+++ .emacs.d/plugins/easy-motion-mode/easy-motion-mode.el
@@ -24,23 +24,68 @@
 ;;
 ;; gsl-mode is a emacs port version of EasyMotion plugin in vim.
 
+
+;;; register as a minor mode
 (or (assq 'easy-motion-mode minor-mode-alist)
     (nconc minor-mode-alist
           (list '(easy-motion-mode easy-motion-mode))))
 
+
+;;; some buffer specific global variable
 (defvar easy-motion-mode nil
   "Mode variable for EasyMotion minor mode.")
+(defvar easy-motion-overlay-foreground-list nil
+  "overlay to cover the selective character")
+(defvar easy-motion-overlay-background nil
+  "overlay to cover the current whole window")
+(defvar easy-motion-move-keys
+  (let ( (i 0)
+         (l nil) )
+    ;; add a-z
+    (setq i ?a)
+    (while (<= i ?z)
+      (setq l (nconc l (list i)))
+      (setq i (+ 1 i)))
+    ;; add A-Z
+    (setq i ?A)
+    (while (<= i ?Z)
+      (setq l (nconc l (list i)))
+      (setq i (+ 1 i)))
+    l)
+  "possible location keys when move cursor") 
+
+
 (make-variable-buffer-local 'easy-motion-mode)
+(make-variable-buffer-local 'easy-motion-overlay-foreground-list)
+(make-variable-buffer-local 'easy-motion-overlay-background)
+
+
+;;; key binding for easy motion mode
 (define-key global-map (kbd "C-x SPC") 'easy-motion-mode)  
 (define-key global-map (kbd "C-c SPC") 'easy-motion-mode)  
 
 
+;;; define the face
+(defface easy-motion-face-background
+  '( (t (:foreground "gray40") )
+     )
+  "face for background of easy motion")
+
+
+(defface easy-motion-face-foreground
+  '( ( ((class color) (background dark))
+       (:foreground "red") )
+     (t (:foreground "gray100") )
+     )
+  "face for foreground of easy motion")
+
 (defvar easy-motion-mode-map 
-  (let ((map (make-keymap)))
+  (let ( (map (make-keymap)) )
+    (dolist (key-code easy-motion-move-keys)
+      (define-key map (make-string 1 key-code) 'easy-motion-move))
     (define-key map [t] 'easy-motion-done)
     map)
   "easy motion key map")
-
 
 
 (defvar easy-motion-mode-hook nil
@@ -49,32 +94,99 @@
 (defvar easy-motion-mode-end-hook nil
   "Funciton(s) to call after stop easy motion mode")
 
+(defun easy-motion-query-char-p ( query-char )
+  "check if the query char is valid"
+  (or (and (>= query-char ?a) (<= query-char ?z))
+      (and (>= query-char ?A) (<= query-char ?Z))))
+
+
+(defun easy-motion-mode-do( query-char )
+  "enter easy motion mode, init easy motion settings"
+  (setq easy-motion-mode " EasyMotion")
+  (force-mode-line-update)
+
+  (let* ((current-window (selected-window))
+         (start-point (window-start current-window))
+         (end-point   (window-end   current-window))
+         (query-char-position-list nil) )
+    (message (format "start point:%d" start-point))
+    (message (format "end point:%d" end-point))
+    ;; create background overlay
+    (setq easy-motion-overlay-background
+          (make-overlay start-point end-point (current-buffer)))
+    (overlay-put easy-motion-overlay-background 'face 'easy-motion-face-background)
+
+    ;; search possible value of query char
+    (save-excursion
+      (goto-char start-point)
+      (let ((case-fold-search nil))
+        (while (search-forward (char-to-string query-char) end-point t)
+          (setq query-char-position-list
+                (nconc query-char-position-list (list (match-beginning 0))))
+          ) ;; while
+        ) ;; let
+      ) ;; save-excursion
+    ;; make foreground overlay
+    (dolist (pos query-char-position-list)
+      (let ( (ol (make-overlay pos (1+ pos) (current-buffer))) )
+        (print ol)
+        (overlay-put ol 'face 'easy-motion-face-foreground)
+        (overlay-put ol 'display "X")
+        (setq easy-motion-overlay-foreground-list
+              (nconc easy-motion-overlay-foreground-list (list ol)))
+        );; let
+      );; dolist
+    (print (length easy-motion-overlay-foreground-list))
+    ) ;; let *
+
+  ;; override the local key map
+  (setq overriding-local-map easy-motion-mode-map)
+  (run-hooks 'easy-motion-mode-hook)
+
+  (add-hook 'mouse-leave-buffer-hook 'easy-motion-done)
+  (add-hook 'kbd-macro-termination-hook 'easy-motion-done)
+  ) ;; defun  
+
+(defun easy-motion-mode ( query-char )
+  "EasyMotion mode"
+  (interactive "cQuery Char:")
+  (if (easy-motion-query-char-p query-char)
+      (easy-motion-mode-do query-char)
+      (error "not invalid query char")
+    ) ;; if
+  ) ;; defun
+
+(defun easy-motion-move ()
+  "move cursor based on user input"
+  (interactive)
+  (let ( (key-code (aref (this-command-keys) 0)) )
+    (when (member key-code easy-motion-move-keys)
+      (message (format "easy-motion-move :%s" (this-command-keys)))
+      );;when
+    );;let
+  (easy-motion-done)
+  )  
+
+
 (defun easy-motion-done()
   "stop easy motion"
   (interactive)
   (setq easy-motion-mode nil)
   (force-mode-line-update)
 
+  (if easy-motion-overlay-background
+      (delete-overlay easy-motion-overlay-background))
+  (setq easy-motion-overlay-background nil)
+
+  (dolist (ol easy-motion-overlay-foreground-list)
+    (delete-overlay ol))
+  (setq easy-motion-overlay-foreground-list nil)
+
   (setq overriding-local-map nil)
   (run-hooks 'easy-motion-mode-end-hook)
 
   (remove-hook 'mouse-leave-buffer-hook 'easy-motion-done)
   (remove-hook 'kbd-macro-termination-hook 'easy-motion-done)
-
-  )
-
-
-(defun easy-motion-mode ()
-  "EasyMotion mode"
-  (interactive)
-  (setq easy-motion-mode " EasyMotion")
-  (force-mode-line-update)
-
-  (setq overriding-local-map easy-motion-mode-map)
-  (run-hooks 'easy-motion-mode-hook)
-
-  (add-hook 'mouse-leave-buffer-hook 'easy-motion-done)
-  (add-hook 'kbd-macro-termination-hook 'easy-motion-done)
 
   )
 
