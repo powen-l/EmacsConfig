3085d642dc1f3ee633ec37a0b0458d2609ca5ee0
winterTTr <winterTTr@gmail.com>
2011-07-14 22:14:03 +0800

    easy-motion-mode: create utils queue type

--- .emacs.d/plugins/easy-motion-mode/easy-motion-mode.el
+++ .emacs.d/plugins/easy-motion-mode/easy-motion-mode.el
@@ -38,13 +38,19 @@
 (defvar easy-motion-overlays nil
   "store all the overlay that created by easy motion
 The first one is background overlay, and the cdr is forground overlay")
+(defvar easy-motion-search-tree nil
+  "we create the search tree for all possible positions")
 
 (make-variable-buffer-local 'easy-motion-mode)
 (make-variable-buffer-local 'easy-motion-overlays)
+(make-variable-buffer-local 'easy-motion-search-tree)
 
+;(defconst easy-motion-move-keys
+;  (nconc (loop for i from ?a to ?z collect i)
+;         (loop for i from ?A to ?Z collect i))
+;  "possible location keys when move cursor") 
 (defconst easy-motion-move-keys
-  (nconc (loop for i from ?a to ?z collect i)
-         (loop for i from ?A to ?Z collect i))
+  (nconc (loop for i from ?1 to ?3 collect i) )
   "possible location keys when move cursor") 
 
 
@@ -80,86 +86,99 @@ The first one is background overlay, and the cdr is forground overlay")
   "Funciton(s) to call after stop easy motion mode")
 
 (defun easy-motion-query-char-p ( query-char )
-  "check if the query char is valid"
-  (or (and (>= query-char ?a) (<= query-char ?z))
-      (and (>= query-char ?A) (<= query-char ?Z))))
+  "check if the query char is valid, we now only use printable ascii char"
+  (and (> query-char #x1F) (< query-char #x7F)) )
 
-
-(defun easy-motion-do( query-char )
-  "enter easy motion mode, init easy motion settings"
+(defun easy-motion-search-candidate( query-string )
+  "search the query-string in current view, and return the candidate position list"
   (let* ((current-window (selected-window))
          (start-point (window-start current-window))
-         (end-point   (window-end   current-window))
-         (candicate-position-list nil) )
-    ;; search candidate position
+         (end-point   (window-end   current-window)) )
     (save-excursion
       (goto-char start-point)
       (let ((case-fold-search nil)) ;; use case sensitive search
-        (setq candicate-position-list
-              (loop while (search-forward (char-to-string query-char) end-point t)
-                    collect (match-beginning 0)))))
+        (loop while (search-forward query-string end-point t)
+                    collect (match-beginning 0))))) )
 
-    (when (null candicate-position-list)
-      (error "There is no such character in current window"))
+(defun easy-motion-construct-search-tree ( candidate-sum )
+  "Constrct the search tree"
+  )
 
-    ;(when (> (length candicate-position-list) 
 
-    ;; create background overlay
-    (setq easy-motion-overlays
-          (list
-           (let ( (ol (make-overlay start-point end-point (current-buffer))) )
-             (overlay-put ol 'face 'easy-motion-face-background)
-             ol)))
+(defun easy-motion-do( query-string )
+  "enter easy motion mode, init easy motion settings"
+  ;; search candidate position
+  (let ((candidate-list (easy-motion-search-candidate query-string)))
+    (cond
+     ;; cannot find any one
+     ((null candidate-list)
+      (error "There is no such character"))
+     ;; we only find one, so move to it directly
+     ((= (length candidate-list) 1)
+      (goto-char (car candidate-list))
+      (message "Move to the only one directly"))
+     ;; more than one, we need to enter easy motion mode
+     (t
+      (setq easy-motion-search-tree
+            (easy-motion-construct-search-tree (length candidate-list)))
+      
+      (print easy-motion-search-tree)
+      ;(easy-motion-update nil)
 
-    ;; make foreground overlay
-    (setq easy-motion-overlays
-          (nconc easy-motion-overlays
-                 (loop for pos in candicate-position-list
-                       collect (let ( (ol (make-overlay pos (1+ pos) (current-buffer))) )
-                                 (overlay-put ol 'face 'easy-motion-face-foreground)
-                                 ;(overlay-put ol 'display (concat "X"))
-                                 ol)))))
+      ;; do minor mode configuration
+      (setq easy-motion-mode " EasyMotion")
+      (force-mode-line-update)
 
-  (easy-motion-update nil)
+      ;; override the local key map
+      (setq overriding-local-map easy-motion-mode-map)
+      (run-hooks 'easy-motion-mode-hook)
 
-  ;; do minor mode configuration
-  (setq easy-motion-mode " EasyMotion")
-  (force-mode-line-update)
+      (add-hook 'mouse-leave-buffer-hook 'easy-motion-done)
+      (add-hook 'kbd-macro-termination-hook 'easy-motion-done)))))
+    ;;; create background overlay
+    ;(setq easy-motion-overlays
+    ;      (list
+    ;       (let ( (ol (make-overlay start-point end-point (current-buffer))) )
+    ;         (overlay-put ol 'face 'easy-motion-face-background)
+    ;         ol)))
 
-  ;; override the local key map
-  (setq overriding-local-map easy-motion-mode-map)
-  (run-hooks 'easy-motion-mode-hook)
+    ;;; make foreground overlay
+    ;(setq easy-motion-overlays
+    ;      (nconc easy-motion-overlays
+    ;             (loop for pos in candidate-position-list
+    ;                   collect (let ( (ol (make-overlay pos (1+ pos) (current-buffer))) )
+    ;                             (overlay-put ol 'face 'easy-motion-face-foreground)
+    ;                             ;(overlay-put ol 'display (concat "X"))
+    ;                             ol)))))
 
-  (add-hook 'mouse-leave-buffer-hook 'easy-motion-done)
-  (add-hook 'kbd-macro-termination-hook 'easy-motion-done))
 
-(defun easy-motion-update ( query-char )
-  "update the overlay based on the easy-motion-move-keys setting and input query"
-  (if (not (null query-char))
-    ;; filter the query-char item
-    (let* ((query-string (make-string 1 query-char))
-           (retain-ols (loop for ol in (cdr easy-motion-overlays)
-                              if (string= (overlay-get ol 'display) query-string)
-                              collect ol))
-           (discard-ols (loop for ol in (cdr easy-motion-overlays)
-                              if (not (memq ol retain-ols))
-                              collect ol)) )
-      (mapcar #'delete-overlay discard-ols)
-      (setq easy-motion-overlays
-            (cons (car easy-motion-overlays) retain-ols))))
-
-  ;; update the display property based on the user input
-  (let* ( (ols (cdr easy-motion-overlays))
-          (ols-length (length ols)) )
-    nil)
-    )
+;(defun easy-motion-update ( query-char )
+;  "update the overlay based on the easy-motion-move-keys setting and input query"
+;  (if (not (null query-char))
+;    ;; filter the query-char item
+;    (let* ((query-string (make-string 1 query-char))
+;           (retain-ols (loop for ol in (cdr easy-motion-overlays)
+;                              if (string= (overlay-get ol 'display) query-string)
+;                              collect ol))
+;           (discard-ols (loop for ol in (cdr easy-motion-overlays)
+;                              if (not (memq ol retain-ols))
+;                              collect ol)) )
+;      (mapcar #'delete-overlay discard-ols)
+;      (setq easy-motion-overlays
+;            (cons (car easy-motion-overlays) retain-ols))))
+; 
+;  ;; update the display property based on the user input
+;  (let* ( (ols (cdr easy-motion-overlays))
+;          (ols-length (length ols)) )
+;    nil)
+;    )
 
 
 (defun easy-motion-mode ( query-char )
   "EasyMotion mode"
   (interactive "cQuery Char:")
   (if (easy-motion-query-char-p query-char)
-      (easy-motion-do query-char)
+      (easy-motion-do (make-string 1 query-char))
       (error "not invalid query char")))
 
 (defun easy-motion-move ()
@@ -188,3 +207,23 @@ The first one is background overlay, and the cdr is forground overlay")
   
 
 
+;;;; ============================================
+;;;; Utilities for easy-motion-mode
+;;;; ============================================
+
+(defstruct em-queue head tail)
+
+(defun em-queue-push (item q)
+  "enqueue"
+  (let ( (head (em-queue-head q) )
+         (tail (em-queue-tail q) )
+         (c (list item) ) )
+    (cond
+     ((null (em-queue-head q))
+      (setf (em-queue-head q) c)
+      (setf (em-queue-tail q) c))
+     (t
+      (setf (cdr (em-queue-tail q)) c)
+      (setf (em-queue-tail q) c)))))
+
+    
