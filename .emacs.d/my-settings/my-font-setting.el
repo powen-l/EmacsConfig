673d9e442eb39c4dab0cf0102cdf2e0f9be36f06
winterTTr <winterTTr@gmail.com>
2011-08-24 21:40:10 +0800

    Complete the font configuration. Now it is ok for chinese, japanese, and ascii. Use consolas as default.

--- .emacs.d/my-settings/my-font-setting.el
+++ .emacs.d/my-settings/my-font-setting.el
@@ -1,12 +1,12 @@
 ;; -*- coding: utf-8 -*-
 
 ;; Font
-(create-fontset-from-fontset-spec
- (concat
-  "-outline-Bitstream Vera Sans Mono-bold-normal-normal-mono-14-*-*-*-c-*-fontset-BVSM,"
-  "chinese-gb2312:-outline-YaHei Consolas Hybrid-normal-normal-normal-mono-*-*-*-*-c-*-gb2312.1980-0,"
-  "sjis:-outline-MS Gothic-normal-normal-normal-mono-13-*-*-*-c-*-jisx0208-sjis" ) )
 ;(create-fontset-from-fontset-spec
+; (concat
+;  "-outline-Bitstream Vera Sans Mono-bold-normal-normal-mono-14-*-*-*-c-*-fontset-BVSM,"
+;  "chinese-gb2312:-outline-YaHei Consolas Hybrid-normal-normal-normal-mono-*-*-*-*-c-*-gb2312.1980-0,"
+;  "sjis:-outline-MS Gothic-normal-normal-normal-mono-13-*-*-*-c-*-jisx0208-sjis" ) )
+;;(create-fontset-from-fontset-spec
 ; (concat
 ;  "-outline-Consolas-normal-normal-normal-mono-18-*-*-*-c-*-fontset-Consolas,"
 ;  "ascii:-outline-Consolas-normal-r-normal-normal-18-*-*-*-c-*-iso8859-1,"
@@ -15,25 +15,22 @@
 ;  "sjis:-outline-MS Gothic-normal-normal-normal-mono-13-*-*-*-c-*-jisx0208-sjis" ) )
 ;(set-default-font "fontset-Consolas")
 
+(create-fontset-from-fontset-spec
+ "-outline-Consolas-normal-normal-normal-mono-16-*-*-*-c-*-fontset-Consolas")
+(set-fontset-font "fontset-Consolas" 'ascii "Consolas-14" nil 'prepend)
+(set-fontset-font "fontset-Consolas" 'han "YaHei Consolas Hybrid-12" nil 'prepend)
+(set-fontset-font "fontset-Consolas" 'kana "MS Gothic-12" nil 'prepend)
+(set-default-font "fontset-Consolas")
+
 (setq default-frame-alist
       (append
        '((font . "fontset-Consolas")) default-frame-alist))
 
-(create-fontset-from-fontset-spec
- "-outline-Consolas-normal-normal-normal-mono-18-*-*-*-c-*-fontset-Consolas")
-(set-fontset-font
- "fontset-Consolas" nil
- "-outline-Consolas-normal-r-normal-normal-18-*-*-*-c-*-iso8859-1" nil 'prepend)
-(set-fontset-font
- "fontset-Consolas" 'chinese-gb2312
- "-outline-YaHei Consolas Hybrid-normal-normal-normal-sans-*-*-*-*-p-*-gb2312*-*" nil 'prepend)
-(set-fontset-font
- "fontset-Consolas" 'chinese-gbk
- "-outline-YaHei Consolas Hybrid-normal-normal-normal-sans-*-*-*-*-p-*-gb2312*-*" nil 'prepend)
-(set-default-font "fontset-Consolas")
+;; test example:
+;; Chinese : 测试
+;; Katakana: わたし
+;; Hirakana: ワタシ
 
 (provide 'my-font-setting)
 
-
-;Emacs字体设置
 
