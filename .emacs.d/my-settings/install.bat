551a964a70eff7c93713e277e0093e6fcd370e5a
winterTTr <winterTTr@gmail.com>
2011-11-14 16:20:56 +0800

    update the install script to emacs24

--- .emacs.d/my-settings/install.bat
+++ .emacs.d/my-settings/install.bat
@@ -1,2 +1,2 @@
 @echo off
-REG ADD "HKCR\*\shell\open with emacs...\command" /ve /d "e:\Tools\emacs-23.3\bin\emacsclientw.exe \"%%1\" -a e:\Tools\emacs-23.3\bin\runemacs.exe"
+REG ADD "HKCR\*\shell\open with emacs...\command" /ve /d "e:\Tools\Emacs\bin\emacsclientw.exe \"%%1\" -a e:\Tools\Emacs\bin\runemacs.exe"
