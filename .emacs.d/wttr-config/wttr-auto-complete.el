73b8719d2f378e4343b0171c15af8164c9b74121
winterTTr <winterTTr@gmail.com>
2012-01-30 22:01:28 +0800

    Some extra setting for auto-complete-clang The vs2010 compiler needs some hach on unsupported feature: __w64

--- .emacs.d/wttr-config/wttr-auto-complete.el
+++ .emacs.d/wttr-config/wttr-auto-complete.el
@@ -30,26 +30,29 @@
 (add-hook 'c++-mode-hook 'wttr/ac-cc-mode-setup)
 
 ;; system specific setting
-(let ((c++-include-path-list (cond
-                              ((string= (system-name) "WINTERTTR-WS")
-                               (list "D:/src/zephyr/perf/OTHERS/STDCPP/INCLUDE"
-                                     "D:/src/zephyr/perf/TOOLS/PUBLIC/ext/crt80/inc"
-                                     "D:/src/zephyr/perf/PUBLIC/COMMON/OAK/INC"
-                                     "D:/src/zephyr/perf/PUBLIC/COMMON/SDK/INC"))
-                              ((string= (system-name) "DONGWANGDSK01")
-                               (list "C:/Program Files/Microsoft SDKs/Windows/v6.1/Include"
-                                     "C:/Program Files/Microsoft SDKs/Windows/v6.1/Include/gl"
-                                     "C:/Program Files/Microsoft SDKs/Windows/v6.1/VC/Include"
-                                     "C:/Program Files (x86)/Microsoft Visual Studio 8/VC/include"
-                                     "C:/Program Files (x86)/Microsoft Visual Studio 8/VC/atlmfc/include"
-                                     "D:/_SRC_/boost/include"))
-                              ((string= (system-name) "WINTERTTR-PC")
-                               (list "D:/Program Files/Microsoft Visual Studio 10.0/VC/include"
-                                     "D:/Program Files/Microsoft Visual Studio 10.0/VC/atlmfc/include"
-                                     "D:/_SRC_/boost/include"))
-                              (t
-                               nil))))
-  (setq ac-clang-flags (mapcar (lambda (x) (concat "-I" x)) c++-include-path-list)))
+(let ((extra-clang-flags (cond
+                          ((string= (system-name) "WINTERTTR-WS")
+                           (list "-ID:/src/zephyr/perf/OTHERS/STDCPP/INCLUDE"
+                                 "-ID:/src/zephyr/perf/TOOLS/PUBLIC/ext/crt80/inc"
+                                 "-ID:/src/zephyr/perf/PUBLIC/COMMON/OAK/INC"
+                                 "-ID:/src/zephyr/perf/PUBLIC/COMMON/SDK/INC"
+                                 "-fmsc-version=1500"))
+                          ((string= (system-name) "DONGWANGDSK01")
+                           (list "-IC:/Program Files/Microsoft SDKs/Windows/v6.1/Include"
+                                 "-IC:/Program Files/Microsoft SDKs/Windows/v6.1/Include/gl"
+                                 "-IC:/Program Files/Microsoft SDKs/Windows/v6.1/VC/Include"
+                                 "-IC:/Program Files (x86)/Microsoft Visual Studio 8/VC/include"
+                                 "-IC:/Program Files (x86)/Microsoft Visual Studio 8/VC/atlmfc/include"
+                                 "-ID:/_SRC_/boost/include"))
+                          ((string= (system-name) "WINTERTTR-PC")
+                           (list "-ID:/Program Files/Microsoft Visual Studio 10.0/VC/include"
+                                 "-ID:/Program Files/Microsoft Visual Studio 10.0/VC/atlmfc/include"
+                                 "-ID:/_SRC_/boost/include"
+                                 "-fmsc-version=1600"
+                                 "-D__w64="))
+                          (t
+                           nil))))
+  (setq ac-clang-flags extra-clang-flags))
 ;(when (string-equal (system-name) "WINTERTTR-WS")
 ;  (setq ac-clang-flags (mapcar (lambda (x) (concat "-I" x)) 
 ;                               (list "D:/src/zephyr/perf/OTHERS/STDCPP/INCLUDE"
