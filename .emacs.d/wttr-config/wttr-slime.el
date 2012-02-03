f83214111bac768f942238ed47f33ff35d1349b3
winterTTr <winterTTr@gmail.com>
2012-02-03 13:40:57 +0800

    Use the windows setting under condition check for slime.

--- .emacs.d/wttr-config/wttr-slime.el
+++ .emacs.d/wttr-config/wttr-slime.el
@@ -2,15 +2,15 @@
 
 ;; we need to add the ccl binary path
 (when (string-equal system-type "windows-nt")
-  (wttr/add-to-exec-path "~/../ccl-1.7-windows"))
+  (wttr/add-to-exec-path "~/../ccl-1.7-windows")
+  (setq inferior-lisp-program
+        (if (getenv "PROGRAMW6432") "wx86cl64.exe" "wx86cl.exe"))
+  (add-to-list 'load-path (expand-file-name "~/../ccl-quicklisp/quicklisp"))
+  (autoload 'slime "slime-helper" nil t))
 
 ;; we use emacs to slime repl
 (when (boundp 'evil-emacs-state-modes)
   (add-to-list 'evil-emacs-state-modes 'slime-repl-mode))
 
-(add-to-list 'load-path (expand-file-name "~/../ccl-quicklisp/quicklisp"))
-(autoload 'slime "slime-helper" nil t)
-(setq inferior-lisp-program
-      (if (getenv "PROGRAMW6432") "wx86cl64.exe" "wx86cl.exe") )
 
 (provide 'wttr-slime)
