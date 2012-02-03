8aa33a4d067a96ae50027284c2e97363dbccad27
winterTTr <winterTTr@gmail.com>
2012-02-03 15:50:37 +0800

    Add wrap region mode, kinda interesting and useful.

--- .emacs.d/wttr-config/wttr-basic.el
+++ .emacs.d/wttr-config/wttr-basic.el
@@ -362,8 +362,20 @@
 
 ;; expand-region
 (add-to-list 'load-path "~/.emacs.d/plugins/expand-region")
-(autoload 'er/expand-region "auto expand region" t)
+(autoload 'er/expand-region "expand-region" "auto expand region" t)
 (global-set-key (kbd "M-2") 'er/expand-region)
+
+;; wrap-region
+(add-to-list 'load-path "~/.emacs.d/plugins/wrap-region")
+(require 'wrap-region)
+(wrap-region-global-mode t)
+; (wrap-region-add-wrappers
+;  '(("$" "$")
+;    ("{-" "-}" "#")
+;    ("/" "/" nil 'ruby-mode)
+;    ("/* " " */" "#" '(java-mode javascript-mode css-mode))
+;    ("`" "`" nil '(markdown-mode ruby-mode))))
+; (add-to-list 'wrap-region-except-modes 'conflicting-mode)
 
 
 (provide 'wttr-basic)
