303954109658d495fca6d236449c901fa45f8bd3
winterTTr <winterTTr@gmail.com>
2012-03-31 10:25:53 +0800

    We always use C++ mode for c/c++ header files.

--- .emacs.d/wttr-config/wttr-minor-modes.el
+++ .emacs.d/wttr-config/wttr-minor-modes.el
@@ -1,12 +1,13 @@
 ;; -*- coding: utf-8 -*-
 
-;; c setting
+;; c c++ mode setting
 (setq-default c-basic-offset 4)
 (defun wttr/cc-mode:basic-setup ()
   "Set setting for cc mode"
   (c-set-style "stroustrup"))
 (add-hook 'c-mode-hook 'wttr/cc-mode:basic-setup)
 (add-hook 'c++-mode-hook 'wttr/cc-mode:basic-setup)
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 ;; server mode
 (server-start)
