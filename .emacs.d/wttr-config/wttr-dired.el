08ccafea4bb574a8a21df6d86c162dbf8e90be7f
unknown <winterTTr@.(none)>
2012-04-12 23:50:41 +0800

    Add setting for dired mode, try to use it.

--- .emacs.d/wttr-config/wttr-dired.el
+++ .emacs.d/wttr-config/wttr-dired.el
@@ -5,8 +5,21 @@
 (setq dired-recursive-deletes t)
 (setq dired-recursive-copies t)
 
+
+;; normal we use the Recycle bin
+(setq delete-by-moving-to-trash t)
+
 ;; use the isearch only for filename
 (setq dired-isearch-filenames t)
+
+
+;; we use extra function
+(require 'dired-x)
+
+;; use wdired
+(require 'wdired)
+(setq wdired-allow-to-change-permissions 'advanced)
+(define-key dired-mode-map (kbd "r") 'wdired-change-to-wdired-mode)
 
 
 (provide 'wttr-dired)
