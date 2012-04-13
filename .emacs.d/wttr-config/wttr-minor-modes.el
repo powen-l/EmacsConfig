bb4abcd9c26f411dd5fe887f9de4be740240e358
unknown <darwinw@WINTERTTR-WS.fareast.corp.microsoft.com>
2012-04-13 17:19:39 +0800

    Add update time of smex (60s).

--- .emacs.d/wttr-config/wttr-minor-modes.el
+++ .emacs.d/wttr-config/wttr-minor-modes.el
@@ -342,6 +342,7 @@
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(smex-auto-update 60)
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
