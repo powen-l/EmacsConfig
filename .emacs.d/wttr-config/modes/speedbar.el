935d92760b173305dfb036494f0cd967dc91451f
winterTTr <winterTTr@gmail.com>
2012-04-18 17:21:33 +0800

    As the cedet is containing the speed bar, we remove separate plugin.

--- .emacs.d/wttr-config/modes/speedbar.el
+++
@@ -1,6 +0,0 @@
-;; -*- coding: utf-8 -*-
-
-;(wttr/plugin:prepend-to-load-path "sr-speedbar-0.1.8")
-;(require 'sr-speedbar)
-;(setq sr-speedbar-right-side nil)
-;(setq speedbar-show-unknown-files nil)
