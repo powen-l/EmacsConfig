16648492ee354981c0a468409f574bb204ff7316
winterTTr <winterTTr@gmail.com>
2012-08-07 13:32:17 +0800

    Change .emacs to .emacs.d/init.el
    This can make all the config into a single folder, so that we don't
    need to (synbol) link .emacs to $HOME folder. Link .emacs.d to $HOME
    is enough.

---
+++ .emacs.d/init.el
@@ -0,0 +1,13 @@
+;; -*- coding: utf-8 -*-
+
+;;;; load base settings
+(add-to-list 'load-path "~/.emacs.d/wttr-config")
+(require 'wttr-font)
+(require 'wttr-basic)
+(require 'wttr-key)
+(require 'wttr-color-theme)
+(require 'wttr-mode-line)
+(require 'wttr-modes)
+
+
+(put 'ido-exit-minibuffer 'disabled nil)
