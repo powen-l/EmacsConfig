c4ba528cf895342f03fac631a232b8e064f309cb
winterTTr <winterTTr@gmail.com>
2012-08-13 17:05:25 +0800

    Update wrap-region and add yasnippet with git url.

--- .emacs.d/wttr-config/modes/yasnippet.el
+++ .emacs.d/wttr-config/modes/yasnippet.el
@@ -2,8 +2,8 @@
 (require 'wttr-utils)
 
 ;; yasnippet
-(wttr/plugin:prepend-to-load-path "yasnippet-0.6.1c")
+(wttr/plugin:prepend-to-load-path "yasnippet")
 (require 'yasnippet)
 (yas/initialize)
-(yas/load-directory (wttr/plugin:expand-file-name "yasnippet-0.6.1c/snippets"))
+(yas/load-directory (wttr/plugin:expand-file-name "yasnippet/snippets"))
 
