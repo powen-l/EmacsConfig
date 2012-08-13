a7404bde65a5a0bec3eed53eeff2bf3c15973657
winterTTr <winterTTr@gmail.com>
2012-08-13 14:42:19 +0800

    Update to actionscript mode 7.2

--- .emacs.d/wttr-config/modes/action-script.el
+++ .emacs.d/wttr-config/modes/action-script.el
@@ -1,5 +1,5 @@
 ;; -*- coding: utf-8 -*-
 
-(wttr/plugin:prepend-to-load-path "action-script-mode")
+(wttr/plugin:prepend-to-load-path "actionscript-mode")
 (autoload 'actionscript-mode "actionscript-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.as$" . actionscript-mode))
