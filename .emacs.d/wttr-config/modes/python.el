1816f0edb4cdc2c5f319d5877a87cb2614cbb805
winterTTr <winterTTr@gmail.com>
2013-01-22 12:01:14 +0800

    Add jedi support for python complete, it seems a great tools than rope.

--- .emacs.d/wttr-config/modes/python.el
+++ .emacs.d/wttr-config/modes/python.el
@@ -4,11 +4,28 @@
 (when wttr/os:windowsp
   (wttr/prepend-to-exec-path "c:/Python27"))
 
-(setq-default py-indent-offset 4)
+;(add-to-list 'python-mode-hook
+;             (lambda () (require 'pycomplete)))
+
+;; python mode
 (wttr/plugin:prepend-to-load-path "python-enhance")
+(setq-default py-indent-offset 4)
+(autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py$\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-(autoload 'python-mode "python-mode" "Python Mode." t)
 (setq python-mode-hook nil)
-(add-to-list 'python-mode-hook
-             (lambda () (require 'pycomplete)))
+
+
+;; jedi suite
+(wttr/plugin:prepend-to-load-path "jedi-suite/ctable")
+(wttr/plugin:prepend-to-load-path "jedi-suite/epc")
+(wttr/plugin:prepend-to-load-path "jedi-suite/deferred")
+(wttr/plugin:prepend-to-load-path "jedi-suite/jedi")
+(setq jedi:setup-keys t)
+
+(autoload 'jedi:setup "jedi" nil t)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (jedi:setup)
+            (jedi:ac-setup)))
+
